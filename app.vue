b0fcd6bdc64f54942f62274f1c9225700dba708b
ollieSk8 <412537738@qq.com>
2022-10-27 10:25:00 +0800

    Initial commit

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <Html :class="`${theme === 'dark' ? 'dark' : ''}`" :lang="locale">
+    <Body
+      class="antialiased duration-300 transition-colors text-gray-800 dark:text-gray-200 bg-white dark:bg-gray-900"
+    >
+      <NuxtLayout>
+        <NuxtLoadingIndicator :height="5" :duration="3000" :throttle="400" />
+        <NuxtPage />
+      </NuxtLayout>
+    </Body>
+  </Html>
+</template>
+
+<script lang="ts" setup>
+import { AppSetup } from './utils/app'
+import { ITheme } from './utils/theme'
+AppSetup()
+const theme = useState<ITheme>('theme.current')
+const locale = useState<string>('locale.setting')
+</script>
